a6b73f86b5ed77c4b2431723f849c57f9a05ad1e
jaydgm <rjayd22@gmail.com>
2023-10-17 17:52:34 -0700

    Add comments, indentation, and string content

--- rps-ui.js
+++ rps-ui.js
@@ -1,11 +1,17 @@
+// variables to keep track of player and comp points
 let playerScore = 0;
 let compScore = 0;
+
+// variable for each selection
 const rock = document.querySelector('button[value="rock"]');
 const paper = document.querySelector('button[value="paper"]');
 const scissor = document.querySelector('button[value="scissor"]');
+// variables for current result
 const result = document.querySelector('#result');
+// variable for final result to X amount of pts
 const finalResult = document.querySelector('#finalResult');
-    
+
+// linking selector buttons to playRound()
 rock.addEventListener('click',function(){
     playRound(`${rock.value}`);
     getScore();
@@ -24,30 +30,30 @@ function getComputerChoice() {
     const computerPick = ['rock', 'paper', 'scissors']
     return computerPick[Math.floor(Math.random()*computerPick.length)];
 }
-    // initiates one round of R.P.S
-    function playRound(playerSelection, computerSelection = getComputerChoice()) {
-        // turns all input to lowercase so that no error when comparing
-        let lowercasePS = playerSelection.toLowerCase();
-        let lowercaseCS = computerSelection.toLowerCase();
-        // case 1 for tie game
-        if (lowercasePS === lowercaseCS) {
-            return result.textContent = "It's a tie";
-        } else if (lowercasePS === 'rock' && lowercaseCS === 'scissors') {
-            // increment playerScore by 1 if player wins
-            playerScore++;
-            return result.textContent = 'player wins! player has points';
-        } else if (lowercasePS === 'scissors' && lowercaseCS === 'paper') {
-            playerScore++;
-            return result.textContent = 'player wins! player has points';
-        } else if (lowercasePS === 'paper' && lowercaseCS === 'rock') {
-            playerScore++;
-            return result.textContent = 'player wins! player has points';
-        } else {
-            // all cases where computer wins, increments compScore by 1
-            compScore++;
-            return result.textContent = 'computer wins! computer has points';
-        }
+// initiates one round of R.P.S
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
+    // turns all input to lowercase so that no error when comparing
+    let lowercasePS = playerSelection.toLowerCase();
+    let lowercaseCS = computerSelection.toLowerCase();
+    // case 1 for tie game
+    if (lowercasePS === lowercaseCS) {
+        return result.textContent = "It's a tie";
+    } else if (lowercasePS === 'rock' && lowercaseCS === 'scissors') {
+        // increment playerScore by 1 if player wins
+        playerScore++;
+        return result.textContent = 'player wins! Rock beats scissors';
+    } else if (lowercasePS === 'scissors' && lowercaseCS === 'paper') {
+        playerScore++;
+        return result.textContent = 'player wins! Scissors beats paper';
+    } else if (lowercasePS === 'paper' && lowercaseCS === 'rock') {
+        playerScore++;
+        return result.textContent = 'player wins! Paper beats rock';
+    } else {
+        // all cases where computer wins, increment compScore by 1
+        compScore++;
+        return result.textContent = `computer wins! ${lowercaseCS} beat ${lowercasePS}`;
     }
+}
 
 // checks score after 5 points
 function getScore() {
